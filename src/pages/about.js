87f9e6a112a093e56fc6f08cef54fe0a3309ca26
eddara <108026279+eddara@users.noreply.github.com>
2023-08-29 14:22:35 +0200

    metadata

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,45 @@
-import { useRouter } from 'next/router';
 import Header from '../components/Header';
 import Image from 'next/image';
 import { aboutContent, eventsContent } from '../../public/data/aboutContent';
+import Head from 'next/head';
 
 const AboutPage = () => {
 
   return (
     <>
+      <Head>
+        <title>About Me - Edda Rabold</title>
+        <meta name="description" content="Learn about Edda Rabold and her design. Contact for collaborations." />
+        
+        {/* Open Graph tags for social media sharing */}
+        <meta property="og:title" content="About Me - Edda Rabold" />
+        <meta property="og:description" content="Learn about Edda Rabold and her design. Contact for collaborations." />
+        <meta property="og:image" content="/images/og-image.jpg" />
+        <meta property="og:url" content="https://www.eddarabold.com/about" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Card tags for Twitter sharing */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="About Me - Edda Rabold" />
+        <meta name="twitter:description" content="Learn about Edda Rabold and her design. Contact for collaborations." />
+        <meta name="twitter:image" content="/images/twitter-image.jpg" />
+        <meta name="twitter:url" content="https://www.eddarabold.com/about" />
+
+        {/* Other meta tags */}
+        <meta name="author" content="Edda Rabold" />
+        <meta name="keywords" content="design, digital production, collaboration, technology" />
+        <meta name="robots" content="all" />
+        <link rel="canonical" href="https://eddarabold.com/about" />
+      </Head>
+
+
       <Header />
-     
+    
 
       <main style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', padding: '1em',marginTop:'5em', justifyContent:'center'}}>
 
       <div style={{ margin: '1em' }}>
-          <Image src="/images/portrait-edda1.jpg" alt="portrait Rabold" width={300} height={400} />
+          <Image priority={true} src="/images/portrait-edda1.jpg" alt="portrait Rabold" width={300} height={400} />
       </div>
 
       <div style={{ margin: '1em', width: '50%' }}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,37 @@
 import Header from "../components/Header"
 import CarouselComponent from '../components/Carousel'
+import Head from 'next/head';
 
 export default function Home() {
 
   return (
      <>
-  <Header /> 
+     <Head>
+        <title>Edda Rabold - Designer</title>
+        <meta name="description" content="Welcome to the portfolio of Edda Rabold, a designer specializing in digital production and unique design processes." />
+        
+        {/* Open Graph tags for social media sharing */}
+        <meta property="og:title" content="Edda Rabold - Designer" />
+        <meta property="og:description" content="Welcome to the portfolio of Edda Rabold, a designer specializing in digital production and unique design processes." />
+        <meta property="og:image" content="/images/og-image.jpg" />
+        <meta property="og:url" content="https://www.eddarabold.com/" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Card tags for Twitter sharing */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Edda Rabold - Designer" />
+        <meta name="twitter:description" content="Welcome to the portfolio of Edda Rabold, a designer specializing in digital production and unique design processes." />
+        <meta name="twitter:image" content="/images/twitter-image.jpg" />
+        <meta name="twitter:url" content="https://www.eddarabold.com/" />
+
+        {/* Other meta tags */}
+        <meta name="author" content="Edda Rabold" />
+        <meta name="keywords" content="design, digital production, technology, portfolio" />
+        <meta name="robots" content="all" />
+        <link rel="canonical" href="https://eddarabold.com" />
+      </Head>
+
+     <Header /> 
 
       <main>
 
